c76d2e3465decd94607d6217b11218968ca056d0
ayau-akk <118360890+ayau-akk@users.noreply.github.com>
2023-01-16 17:21:45 +0600

    Add files via upload

---
+++ hw3.1.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+
+class Car,
+{ 
+public  $marka;
+public  $driver;
+public  $enge;
+public  $carClass;
+
+public function start() {
+        echo "Поехали<br>";
+    }
+    
+public function stop() {
+        echo "Останавливаемся<br>";
+    }
+    
+public function turnRight() {
+        echo "Поворот направо<br>";
+    }
+    
+public function turnLeft() {
+        echo "Поворот налево<br>";
+    }
+    
+public function __toString() {
+        return $this->marka;
+        return $this->driver;
+        return $this->enge;
+        return $this->carClass;
+    }
+    
+}
+
+class Lorry extends Car {
+   public $carrying;
+}
+
+class sportCar extends Car {
+   public $speed;
+}
+ 
+$car = new Car();
+print_r($car);
+
+
+class Engine,
+{
+public $power;
+public $company;
+
+}
+ 
+$engine = new Engine();
+print_r($engine);
+
+
+class Driver,
+{
+public $name;
+public $experience;
+}
+ 
+$driver = new Driver();
+print_r($driver);
+
+
+
+?> 
+
+
+
+
+</body>
+</html>
